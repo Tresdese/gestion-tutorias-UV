63ea17d446cffbcfe288023eec057cd7c57ae70a
tresdese <super_diegobros@hotmail.com>
2026-04-17 15:19:59 -0600

    merge

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.sistemagestiontutorias {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.sistemagestiontutorias to javafx.fxml;
-    exports com.mycompany.sistemagestiontutorias;
-}
